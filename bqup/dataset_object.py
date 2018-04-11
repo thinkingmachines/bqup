8a427a4999f5db3b2dc048b35c57ea28bb48a3d5
Mark Steve Samson <marksteve@thinkingmachin.es>
2018-04-11 11:14:49 +0800

    Python package

--- bqup/dataset_object.py
+++ bqup/dataset_object.py
@@ -1,6 +1,7 @@
-from util import run
+from bqup.util import run
 
-class DatasetObject:
+
+class DatasetObject(object):
 
   def __init__(self, project, dataset, name, type):
     self.project = project
@@ -11,12 +12,13 @@ class DatasetObject:
 
   def get_source(self):
     if self.type == "VIEW":
-      self.source = run("bq show --view %s:%s.%s"%(self.project.name, self.dataset.name, self.name))
+      self.source = run("bq show --view {}:{}.{}".format(
+        self.project.name, self.dataset.name, self.name))
       return self.source
 
   @classmethod
   def list(cls, project, dataset):
     return list(map(
       lambda x: DatasetObject(project, dataset, *(x.split())),
-      run("bq ls %s:%s"%(project.name, dataset.name)).split("\n")[2:-1]
+      run("bq ls {}:{}".format(project.name, dataset.name)).split("\n")[2:-1]
     ))
--- bqup/util.py
+++ bqup/util.py
@@ -1,5 +1,6 @@
 import subprocess
 
+
 def run(cmd):
   """Runs a shell command and returns the output as a string."""
   return subprocess.check_output(cmd.split(" ")).decode("UTF-8")
