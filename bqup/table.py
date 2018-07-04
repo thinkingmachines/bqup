f808ea67f1218168102cf00eca6865f878dc7064
iman <iman@syvlabs.com>
2018-07-04 11:52:48 +0800

    Add file extension + some refactoring

--- bqup/table.py
+++ bqup/table.py
@@ -1,7 +1,8 @@
 import json
+from os import path
 
-class Table():
 
+class Table:
     view_query = ''
     schema = []
 
@@ -12,23 +13,39 @@ class Table():
         self.table_type = bq_table.table_type
         dataset.tables.append(self)
 
-        if (self.table_type == 'VIEW'):
+        if self.table_type == 'VIEW':
             # To support multiple version of google-cloud-bigquery
             ref = bq_table if hasattr(bq_table, 'path') else bq_table.reference
             table = dataset.project.client.get_table(ref)
             self.view_query = table.view_query
-        elif (self.table_type == 'TABLE'):
+        elif self.table_type == 'TABLE':
             if export_schema:
                 table = dataset.project.client.get_table(bq_table)
                 self.schema = list(map(lambda x: x.to_api_repr(), table.schema))
-        elif (self.table_type == 'EXTERNAL'):
+        elif self.table_type == 'EXTERNAL':
             if export_schema:
                 table = dataset.project.client.get_table(bq_table)
                 self.schema = list(map(lambda x: x.to_api_repr(), table.schema))
         else:
-            print('Unrecognized table type: {}'.format(self.table_type))
-            exit()
+            raise ValueError('Unrecognized table type: {}'.format(self.table_type))
 
+    def _get_export_file_extension(self):
+        """
+        Get the file extension for the export file of this table.
+        :return: the file extension (i.e. "sql")
+        """
+        if self.table_type == 'VIEW':
+            return 'sql'
+        else:
+            return 'json'
+
+    def _get_export_table_path(self, dataset_dir):
+        """
+        Get the export path for this table
+        :return: the export path
+        """
+        table_file_name = '{}.{}.{}'.format(self.table_id, self.table_type.lower(), self._get_export_file_extension())
+        return path.join(dataset_dir, table_file_name)
 
     def to_file_contents(self):
         return self.view_query or json.dumps(self.schema)
@@ -38,7 +55,6 @@ class Table():
             self.table_type, self.table_id, len(self.view_query)))
 
     def export(self, dataset_dir):
-        table_path = '{}/{}.{}'.format(dataset_dir,
-                                       self.table_id, self.table_type.lower())
+        table_path = self._get_table_path(dataset_dir)
         with open(table_path, 'w') as f:
             f.write(self.to_file_contents())
