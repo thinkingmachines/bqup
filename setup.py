48e45487fb703cf25ad1fc05e66537d9b3f9f280
Pepe Bawagan <syk0saje@gmail.com>
2019-11-26 11:55:32 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='bqup',
-    version='0.0.5',
+    version='0.0.6',
     description='BigQuery backup scripts',
     long_description=readme,
     long_description_content_type='text/markdown',
