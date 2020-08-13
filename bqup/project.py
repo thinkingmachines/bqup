0648ab47cda1ceb41e4a8830a18c27022abb3d53
Joaq <joaquin.almirante@gmail.com>
2020-08-14 00:32:33 +0800

    Add regex parameter filter to backup specific datasets

--- bqup/project.py
+++ bqup/project.py
@@ -3,7 +3,7 @@ import shutil
 from google.cloud import bigquery
 from functools import partial
 from bqup.dataset import Dataset
-
+import re
 
 class Project():
     """Project that enables content exploration and exporting.
@@ -27,12 +27,20 @@ class Project():
 
     datasets = []
 
-    def __init__(self, project_id=None, export_schema=False, include_routines=False):
+    def __init__(self, project_id=None, export_schema=False, include_routines=False, regex_pattern=None):
         self.client = bigquery.Client(project_id)
         self.project_id = self.client.project
+        self.regex_pattern = regex_pattern
+
         print('Loading project {}...'.format(self.project_id))
         self.datasets = list(
-            map(partial(Dataset, self, export_schema, include_routines), self.client.list_datasets()))
+            map(partial(Dataset, self, export_schema, include_routines), filter(self.__is_match_dataset_item, self.client.list_datasets())))
+
+    def __is_match_dataset_item(self, dataset_item):
+        """Returns match if regex pattern is found in dataset_id"""
+        if not self.regex_pattern: 
+            return True 
+        return re.match(self.regex_pattern, dataset_item.dataset_id)
 
     def print_info(self):
         """Displays names of datasets, tables, and views as a hierarchical tree."""
